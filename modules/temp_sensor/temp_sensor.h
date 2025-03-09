673d877672a9a7be8bf918914aae8827be859603
MFitzmaurice27 <fitzmaum@union.edu>
2025-03-09 16:38:46 -0400

    Update temp_sensor.h

---
+++ modules/temp_sensor/temp_sensor.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+#ifndef _TEMP_SENSOR_H_
+#define _TEMP_SENSOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void temp_sensor_init();
+
+void temp_sensor_update();
+
+float getTempAvg();
+
+//=====[#include guards - end]=================================================
+
+#endif // _SERVO_H_
