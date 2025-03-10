b010bb0e2255e90e49ce919826c02b489eddb0be
AdvityaCode <singha4@union.edu>
2025-03-10 10:31:38 -0400

    Update sanitization_proof.cpp

--- modules/sanitization_proof/sanitization_proof.cpp
+++ modules/sanitization_proof/sanitization_proof.cpp
@@ -1,38 +1,12 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include "gas_sensor.h"
 
-AnalogIn gasSensor(A0);
-DigitalOut SanitizationProved(LED1);
+DigitalOut SanitizationProved(LED2);
 
-#define SANITIZED_THRESHOLD 0.4f
-UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
-
-void pcSerialComStringWrite( const char* str )
-{
-    uartUsb.write( str, strlen(str) );
-}
-
-float sanitizationProofUpdate(){
-
-    while (true) {
-        float sensorValue = gasSensor.read();  // Read analog value (0.0 - 1.0)
-
-        if (sensorValue > SANITIZED_THRESHOLD) {
-            SanitizationProved = 1;  // Turn on LED when sanitized air is detected
-        } else {
-            SanitizationProved = 0;  // Turn off LED otherwise
-        }
-
-        ThisThread::sleep_for(500ms);
-    return sensorValue;
+void sanitizationProofUpdate(){
+    if (!gasSensorRead()) {
+        SanitizationProved = ON;
     }
 }
 
-void printToMon2() {
-    char str[100] = "";
-    float gasStatus = gasSensor.read();
-    sprintf ( str, "Distance: %.2f cm\n", gasStatus,
-                    sanitizationProofUpdate());
-    pcSerialComStringWrite( str );  
-
-}
